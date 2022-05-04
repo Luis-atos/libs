e4951c312a80a0ef79f3febf16954ce3eff16966
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-05-04 14:56:04 +0200

    Update libreria.groovy

--- libreria.groovy
+++ libreria.groovy
@@ -3,9 +3,11 @@ def version = '1.0'
 //notificación para todos los JF de ciclos de contrucción, despliegue y pruebas de seguridad
 //********************************************************************************************
 def metodo1() {
+  echo" ************* METODO 1 ********************"
   echo "metodo 1..."
  }
  def metodo2() {
+   echo" ************* METODO 2 ********************"
   echo "metodo 2..."
  }
  return this
